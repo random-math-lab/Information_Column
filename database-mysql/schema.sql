7b6795c3bbffef906cb1667857d758bd0d57f4a9
omega1984 <omegareborn2019@gmail.com>
2019-08-22 16:02:00 -0700

    installed faker.js

--- database-mysql/schema.sql
+++ database-mysql/schema.sql
@@ -5,3 +5,14 @@ create table guests (
   primary key(id)
 );
 
+
+INSERT INTO guests (price, availability) values (70.00, true);
+INSERT INTO guests (price, availability) values (75.99, false);
+INSERT INTO guests (price, availability) values (79.99, true);
+INSERT INTO guests (price, availability) values (69.99, false);
+INSERT INTO guests (price, availability) values (72.99, true);
+INSERT INTO guests (price, availability) values (73.99, false);
+INSERT INTO guests (price, availability) values (74.99, true);
+INSERT INTO guests (price, availability) values (75.99, false);
+INSERT INTO guests (price, availability) values (76.99, true);
+INSERT INTO guests (price, availability) values (77.99, false);
