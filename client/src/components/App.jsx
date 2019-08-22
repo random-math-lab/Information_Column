af5c99398633470c08002e33c256bb628af8cdac
omega1984 <omegareborn2019@gmail.com>
2019-08-21 23:34:40 -0700

    start working on draft reservation box

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Let's Go
+      <div className="booking">
+        <h3 className="booking-price">$ 80 <span className="booking-per-night">per night</span></h3>
+        <hr></hr>
+        <div className="form-group">
+          <label htmlFor="dates">Dates</label>
+          <DateRangePicker open="left" containerStyles={{display: "block"}}>
+            <input id="dates" type="text" className="form-control"></input>
+          </DateRangePicker>
+        </div>
+        <div className='form-group'>
+          <label htmlFor="guests">Guests</label>
+          <input type="number" className="form-control" id="guests" aria-describedby="emailHelp" placeholder=""></input>
+        </div>
+        <button className="btn btn-bwm btn-confirm btn-block">Reserve place now</button>
+        <hr></hr>
+        <p className="booking-note-title">People are interested into this house</p>
+        <p className="booking-note-text">More than 500 people checked this rental in last month</p>
       </div>
     )
   }
