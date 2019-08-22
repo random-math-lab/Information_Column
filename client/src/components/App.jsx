2121765c12181a9c3b73c05119a80c15e2027e28
omega1984 <omegareborn2019@gmail.com>
2019-08-21 19:14:27 -0700

    getting started

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,26 +5,26 @@ export default class App extends Component {
     return (
       <div>
         <div>
-        <form className="px-4 py-3">
-          <div>
-            <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
-          </div>
-          <div>
-            <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
-          </div>
-          <div className="form-group">
-            <div className="form-check">
-              <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
-              <label htmlFor="dropdownCheck" className="form-check-label">Remember</label>
+          <form className="px-4 py-3">
+            <div>
+              <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
             </div>
-          </div>
+            <div>
+              <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
+            </div>
+            <div className="form-group">
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" id="dropdownCheck"/>
+                <label htmlFor="dropdownCheck" className="form-check-label">Remember</label>
+              </div>
+            </div>
             <button type="submit" className="btn btn-primary">Sign in</button>
-        </form>
-        <div className="dropdown-divider"></div>
-        <a className="dropdown-item" href="#">New around here? Sign up</a>
-        <a className="dropdown-item" href="#">Forgot password?</a>
+          </form>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">New around here? Sign up</a>
+          <a className="dropdown-item" href="#">Forgot password?</a>
+        </div>
       </div>
-    </div>
     )
   }
 }
