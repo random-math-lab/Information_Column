befda42a1771e6cc1e375fb1340ba09e5f29438d
omega1984 <omegareborn2019@gmail.com>
2019-08-23 13:50:22 -0700

    per unit price complete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,17 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <h3 className="booking-price">$ 80 <span className="booking-per-night">per night</span></h3>
-        <hr></hr>
         <DatePickerForm />
-        <div className='form-group'>
-          <label htmlFor="guests">Guests</label>
-          <input type="number" className="form-control" id="guests" aria-describedby="emailHelp" placeholder=""></input>
-        </div>
-        <button className="btn btn-bwm btn-confirm btn-block btn btn-danger">Reserve</button>
-        <hr></hr>
-        <p className="booking-note-title">People are interested into this house</p>
-        <p className="booking-note-text">More than 500 people checked this rental in last month</p>
       </div>
     )
   }
