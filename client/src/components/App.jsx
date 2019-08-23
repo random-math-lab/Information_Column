bf09beb676f0bb1e0c35f5faca8f404351b1db32
omega1984 <omegareborn2019@gmail.com>
2019-08-23 12:30:39 -0700

    move things around

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,44 +1,19 @@
 import React, { Component } from 'react';
-import DateRangePicker from 'react-bootstrap-daterangepicker';
-import $ from 'jquery';
+import DatePickerForm from './DatePickerForm.jsx'
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      guests: []
-    }
-  }
-
-  componentDidMount(){
-    $.ajax({
-      url: '/api/guests',
-      type: 'GET',
-      success: (data) =>{
-        console.log("received data from server", data);
-        this.setState({
-          guests: data
-        })
-      }
-    })
-  }
 
   render() {
     return (
       <div className="container">
         <h3 className="booking-price">$ 80 <span className="booking-per-night">per night</span></h3>
         <hr></hr>
-        <div className="form-group">
-          <label htmlFor="dates">Dates</label>
-          <DateRangePicker open="left" containerStyles={{display: "block"}}>
-            <input id="dates" type="text" className="form-control"></input>
-          </DateRangePicker>
-        </div>
+        <DatePickerForm />
         <div className='form-group'>
           <label htmlFor="guests">Guests</label>
           <input type="number" className="form-control" id="guests" aria-describedby="emailHelp" placeholder=""></input>
         </div>
-        <button className="btn btn-bwm btn-confirm btn-block">Reserve place now</button>
+        <button className="btn btn-bwm btn-confirm btn-block btn btn-danger">Reserve</button>
         <hr></hr>
         <p className="booking-note-title">People are interested into this house</p>
         <p className="booking-note-text">More than 500 people checked this rental in last month</p>
