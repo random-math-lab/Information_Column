a5f60c66ca8205fcef2f9c48611dfdde70424b5e
omega1984 <omegareborn2019@gmail.com>
2019-08-23 11:35:34 -0700

    server running

--- server/server.js
+++ server/server.js
@@ -3,24 +3,24 @@ const bodyParser = require('body-parser');
 const db = require('../database');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 
-// app.get('/api/guests', (req, res) =>{
-//   db.query(`SELECT * FROM guests`, (err, data) =>{
-//     if (err){
-//       console.log("retrieving data from database failed", err);
-//     }else{
-//       console.log("here is the data from database: ", data);
-//       res.send(data);
-//     }
-//   })
-// })
+app.get('/api/guests', (req, res) =>{
+  db.query(`SELECT * FROM guests`, (err, data) =>{
+    if (err){
+      console.log("retrieving data from database failed", err);
+    }else{
+      console.log("here is the data from database: ", data);
+      res.send(data);
+    }
+  })
+})
 
 
 
