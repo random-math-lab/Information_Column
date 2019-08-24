a6e9ce881d542a252cb898ce651ba00bc1214dd7
omega1984 <omegareborn2019@gmail.com>
2019-08-24 16:10:47 -0700

    redo everything

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import Reservation from './Reservation.jsx'
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <Reservation />
+        Let's Go
       </div>
     )
   }
