dd34563f6ac1ecda197b20e18953a30562eedd37
omega1984 <omegareborn2019@gmail.com>
2019-08-24 21:38:21 -0700

    working on calendar making some progress

--- client/src/components/CloseButton.jsx
+++ client/src/components/CloseButton.jsx
@@ -1,11 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default class CloseButton extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+const StyledCloseButtonWrapper = styled.div`
+  margin: 0px 0px 24px;
+`;
+
+const StyledCloseButton = styled.button`
+  padding: 8px;
+  margin: -8px;
+  cursor: pointer;
+  background: transparent;
+  outline: 0px;
+  border-width: 1px;
+  border-style: initial;
+  border-radius: 100%;
+  border-image: initial;
+  border-color: initial;
+  
+  :active {
+    box-shadow: rgb(216, 216, 216) 0 0 4px 2px;
+  };
+`;
+
+const StyledSVG = styled.svg`
+  display: block;
+  fill: rgb(118, 118, 118);
+`;
+
+const CloseButton = () => (
+  <StyledCloseButtonWrapper>
+    <StyledCloseButton>
+      <StyledSVG viewBox="0 0 24 24" role="img" focusable="false" height="16px" width="16px">
+        <path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fillRule="evenodd" />
+      </StyledSVG>
+    </StyledCloseButton>
+  </StyledCloseButtonWrapper>
+);
+
+export default CloseButton;
--- client/src/components/book.jsx
+++ client/src/components/book.jsx
@@ -1,11 +1,74 @@
-import React, { Component } from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default class book extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+const StyledBookForm = styled.form`
+`;
+
+const StyledBookButton = styled.button`
+  display: inline-block;
+  position: relative;
+  text-align: center;
+  transition-property: background, border-color, color;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-out;
+  cursor: pointer;
+  width: 100%;
+  min-width: 71.1935px;
+  margin: 0px;
+  text-decoration: none;
+  border-radius: 4px;
+  border-width: 2px;
+  border-style: solid;
+  background: rgb(255, 90, 95);
+  border-color: transparent;
+  align-items: flex-start;
+  box-sizing: border-box;
+  border-image: initial;
+  text-rendering: auto;
+  word-spacing: normal;
+  text-indent: 0px;
+  text-shadow: none;
+  font: 400 11px system-ui;
+  margin: 0;
+  padding: 10px 22px 10px 22px;
+
+  :active {
+    background: rgb(223, 60, 71);
   }
-}
+
+  :focus {
+    outline: -webkit-focus-ring-color auto 5px;
+  };
+`;
+
+const StyledButtonText = styled.div`
+  overflow-wrap: break-word;
+  font-family: 'Quicksand', sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1.375em;
+  color: rgb(255, 255, 255);
+  margin: 0px;
+`;
+
+const StyledChargeText = styled.div`
+  margin-top: 8px;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, Helvetica Neue, sans-serif;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 1.33333em;
+  color: #484848;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+`;
+
+const Book = () => (
+  <StyledBookForm>
+    <StyledBookButton>
+      <StyledButtonText>Book</StyledButtonText>
+    </StyledBookButton>
+    <StyledChargeText>You won't be charged yet</StyledChargeText>
+  </StyledBookForm>
+);
+
+export default Book;
