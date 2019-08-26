707acb25f430567ed6b34b52b7819df4c5f7d691
omega1984 <omegareborn2019@gmail.com>
2019-08-26 16:25:58 -0700

    working on dates dropdown

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -15,25 +15,27 @@ const StyledRow = styled.tr`
 `;
 
 const buildMonth = (firstDayOfMonth, daysInMonth) => {
-  const daysArray = Array(firstDayOfMonth);
+  const daysArray = Array(firstDayOfMonth); // firstDayOfMonth determines the length of daysArray
   const monthArray = [];
   const dayPadding = { date: null, isBlank: true };
 
-  daysArray.fill(dayPadding);
+  daysArray.fill(dayPadding); // array.fill(value, start, end), value is the length of daysArray
+  // if length is 0, daysArray fills no dayPadding, else, the amount of dayPadding equals to the length of daysArray will be filled
+  // if firstDayOfMonth is 3, then => [{date: null, isBlank: true}, {date: null, isBlank: true}, {date: null, isBlank: true}, {date: 1, isBlank: false}]
 
-  for (let i = 1; i <= daysInMonth; i += 1) {
+  for (let i = 1; i <= daysInMonth; i += 1) {  // fill up the daysArray with the amount of days in the month
     daysArray.push({ date: i, isBlank: false });
   }
 
   const daysToPad = 7 - (daysArray.length % 7);
 
-  for (let i = 0; i < daysToPad; i += 1) {
+  for (let i = 0; i < daysToPad; i += 1) { // push dayPaddings to the end of daysArray
     daysArray.push(dayPadding);
   }
 
-  for (let i = 0; i < daysArray.length / 7; i += 1) {
-    const week = daysArray.slice(i * 7, (i + 1) * 7);
-    monthArray.push(week);
+  for (let i = 0; i < daysArray.length / 7; i += 1) { // push { week } into monthArray
+    const week = daysArray.slice(i * 7, (i + 1) * 7); // week === [{dayPadding}, {dayPadding}, {dayPadding}, ...]
+    monthArray.push(week); // monthArray === [[week], [week], [week], ...]
   }
 
   return monthArray;
