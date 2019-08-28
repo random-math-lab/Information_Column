163c81bdae0a382892b8f03b427edf787abb5640
omega1984 <omegareborn2019@gmail.com>
2019-08-27 17:16:32 -0700

    make price component working

--- client/src/components/Price.jsx
+++ client/src/components/Price.jsx
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
-export default class Price extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+const StyledPrice = styled.div`
+  display: flex;
+  line-height: 10px;
+  padding: 0px 16px 0px 0px;
+  align-items: baseline;
+  align-content: flex-end;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+`;
+
+const StyledPriceAmt = styled.div`
+  font-size: 22px;
+  overflow-wrap: break-word;
+  font-family: 'Quicksand', sans-serif;
+  font-weight: 800;
+  line-height: 1.44444em;
+  color: rgb(72, 72, 72);
+  margin: 0px;
+`;
+
+const StyledPricePerNight = styled.div`
+  font-family: 'Quicksand', sans-serif;
+  font-size: 12px;
+  font-weight: 600;
+  line-height: 1.33333em;
+  color: rgb(72, 72, 72);
+  margin: 0px;
+  line-height: 10px;
+  padding-left: 4px;
+`;
+
+const Price = (props) => {
+  const { price } = props;
+
+  return (
+    <StyledPrice>
+      <StyledPriceAmt>${price}</StyledPriceAmt>
+      <StyledPricePerNight>per night</StyledPricePerNight>
+    </StyledPrice>
+  );
+};
+
+export default Price;
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -9,3 +9,4 @@ export default class Reviews extends Component {
     )
   }
 }
+
